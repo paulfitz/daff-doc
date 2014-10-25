09a921d9a8c0ce911c620232a285843795db019d
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-10-25 17:54:22 +0200

    use chxdoc

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 default:
+	sleep 1
 	./compile.sh
-	echo "Done"
+	@echo "Done"
+
+watch:
+	make
+	while inotifywait -e modify chxdoc.xml compile.sh ../daff/coopy/*.hx templates/daff/*.mtt ; do make ; done
--- compile.sh
+++ compile.sh
@@ -9,5 +9,8 @@ fi
 
 rm -f coopy
 ln -s ../daff/coopy coopy
-haxe -xml doc.xml ../daff/language/js.hxml
-haxedoc doc.xml -f coopy
+haxe -xml doc.xml ../daff/language/php.hxml
+# haxedoc doc.xml -f coopy
+haxelib run chxdoc --config=chxdoc.xml -o . --developer=false -f doc.xml
+rm -rf php_bin __chxdoctmp
+rm -f doc.xml
